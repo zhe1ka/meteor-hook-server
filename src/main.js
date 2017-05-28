a6711591c66114f84534c9110e924bc1d66d362d
Zhe1ka <zheka.dzecina@gmail.com>
2017-05-28 14:25:02 +0300

    zhe1ka: publish the package

---
+++ src/main.js
@@ -0,0 +1,33 @@
+var groupMethods = function(hook, methods) {
+    for (let method in methods) {
+        if (methods.hasOwnProperty(method)) {
+            var oldFunc = methods[method];
+
+            methods[method] = function() {
+                var params = Array.prototype.slice.call(arguments),
+                    res = hook.call(this);
+
+                if (res) {
+                    params.push(res);
+
+                    oldFunc.apply(this, params);
+                }
+            };
+        }
+    }
+
+    Meteor.methods(methods);
+};
+
+var checkAuthUser = function() {
+    return this.userId;
+};
+
+var AuthMethods = function(methods) {
+    groupMethods(checkAuthUser, methods);
+};
+
+module.exports = {
+    groupMethods: groupMethods,
+    checkAuthUser: checkAuthUser
+};
